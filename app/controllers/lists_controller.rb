6e810f28dd5e04e6d7b5949830e55cfd7172b58f
Ubuntu <ubuntu@ip-172-31-40-88.ap-northeast-1.compute.internal>
2026-06-06 01:21:31 +0000

    6章まで完了

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,40 @@
 class ListsController < ApplicationController
   def new
+    # Viewに当たすためのインスタンス変数に空のmodelオブジェクトを作成する。
+    @list = List.new
   end
 
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+
+  def  create
+    # 1 &2. データを受け取り新規登録するためのインスタンス作成
+    list = List.new(list_params)
+    # 3. データをデータベースに保存するためのsaveメソッド実行
+    list.save
+    # 4. 詳細画面へのリダイレクト
+    redirect_to list_path(list.id)
+  end
+
+  private
+  #ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body, :image)
   end
 end
